aaea04f4703062144ddd00ac50be6bbbd73368f5
Jaime <jaime.chapinal@gmail.com>
2022-07-04 23:29:52 +0200

    Testing new filters

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-var allMarkers = [];
-var markersCruzcampo = [];
-var markersMahou = [];
-var markersEsparte = [];
-var markersAguila = [];
+let dataCsv = null;
+var map = null;
+var markers = [];
+var markersLayer = new L.LayerGroup();
+
+var filterCruzcampo = document.getElementById("filterCruzcampo");
+var filterMahou = document.getElementById("filterMahou");
+var filterEstrellaGalicia = document.getElementById("filterEstrellaGalicia");
+var filterEsparte = document.getElementById("filterEsparte");
+
+var filterUnknown = document.getElementById("filterUnknown");
+
+// var allMarkers = [];
+// var markersCruzcampo = [];
+// var markersMahou = [];
+// var markersEsparte = [];
+// var markersAguila = [];
 
 const file = "./data/conil.csv";
 $.get(file, function (csvString) {
@@ -11,9 +23,23 @@ $.get(file, function (csvString) {
     dynamicTyping: true,
   }).data;
 
+  dataCsv = data;
+
+  updateMap(dataCsv);
+});
+
+const updateMap = (data) => {
+  const isCruzcampoEnabled = filterCruzcampo.checked;
+  const isMahouEnabled = filterMahou.checked;
+  const isEstrellaGaliciaEnabled = filterEstrellaGalicia.checked;
+  const isEsparteEnabled = filterEsparte.checked;
+  const isUnknownEnabled = filterUnknown.checked;
+
   for (var i in data) {
     var row = data[i];
     if (row.lat && row.lon) {
+      let shouldAddToMap = false;
+
       const popupHtml = getPopupHtml(row);
       if (popupHtml) {
         const hasBeerData = popupHtml.includes("<li>");
@@ -26,21 +52,44 @@ $.get(file, function (csvString) {
           icon: myIcon,
         }).bindPopup(popupHtml);
 
+        // if (hasBeerData) {
+        //   if (popupHtml.includes("Cruzcampo")) {
+        //     markersCruzcampo.push(marker);
+        //   }
+        //   if (popupHtml.includes("Mahou")) {
+        //     markersMahou.push(marker);
+        //   }
+        //   if (popupHtml.includes("Esparte")) {
+        //     markersEsparte.push(marker);
+        //   }
+        //   if (popupHtml.includes("Aguila")) {
+        //     markersAguila.push(marker);
+        //   }
+        // } else {
+        //   allMarkers.push(marker);
+        // }
         if (hasBeerData) {
-          if (popupHtml.includes("Cruzcampo")) {
-            markersCruzcampo.push(marker);
+          if (isCruzcampoEnabled && popupHtml.includes("Cruzcampo")) {
+            shouldAddToMap = true;
           }
-          if (popupHtml.includes("Mahou")) {
-            markersMahou.push(marker);
+          if (isMahouEnabled && popupHtml.includes("Mahou")) {
+            shouldAddToMap = true;
           }
-          if (popupHtml.includes("Esparte")) {
-            markersEsparte.push(marker);
+          if (isEsparteEnabled && popupHtml.includes("Esparte")) {
+            shouldAddToMap = true;
           }
-          if (popupHtml.includes("Aguila")) {
-            markersAguila.push(marker);
+          if (
+            isEstrellaGaliciaEnabled &&
+            popupHtml.includes("Estrella Galicia")
+          ) {
+            shouldAddToMap = true;
           }
         } else {
-          allMarkers.push(marker);
+          shouldAddToMap = isUnknownEnabled;
+        }
+
+        if (shouldAddToMap) {
+          markersLayer.addLayer(marker);
         }
       }
 
@@ -59,21 +108,11 @@ $.get(file, function (csvString) {
     }
   }
 
-  let baseGroup = L.layerGroup(allMarkers);
-  let groupCruzcampo = L.layerGroup(markersCruzcampo);
-  let groupMahou = L.layerGroup(markersMahou);
-  let groupEsparte = L.layerGroup(markersEsparte);
-  let groupAguila = L.layerGroup(markersAguila);
-
-  // var OpenStreetMap_Mapnik_tileLayer = L.tileLayer(
-  //   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //   {
-  //     maxZoom: 19,
-  //     attribution:
-  //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  //   }
-  // );
-  // var tileLayer = { BaseLayer: OpenStreetMap_Mapnik_tileLayer };
+  // let baseGroup = L.layerGroup(allMarkers);
+  // let groupCruzcampo = L.layerGroup(markersCruzcampo);
+  // let groupMahou = L.layerGroup(markersMahou);
+  // let groupEsparte = L.layerGroup(markersEsparte);
+  // let groupAguila = L.layerGroup(markersAguila);
 
   var Esri_WorldStreetMap = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
@@ -85,74 +124,72 @@ $.get(file, function (csvString) {
   //controlLayers.addBaseLayer(Esri_WorldStreetMap, "Esri_WorldStreetMap");
   var tileLayer = { BaseLayer: Esri_WorldStreetMap };
 
-  var map = L.map("map", {
-    center: [36.3, -6.1],
-    zoom: 13, // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
-    minZoom: 11,
-    scrollWheelZoom: true,
-    tap: false,
-    layers: [
-      tileLayer["BaseLayer"],
-      baseGroup,
-      groupCruzcampo,
-      groupMahou,
-      groupEsparte,
-      groupAguila,
-    ], //ch
-  });
+  // map = L.map("map", {
+  //   center: [36.3, -6.1],
+  //   zoom: 13, // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
+  //   minZoom: 11,
+  //   scrollWheelZoom: true,
+  //   tap: false,
+  //   layers: [
+  //     tileLayer["BaseLayer"],
+  //     //   baseGroup,
+  //     //   groupCruzcampo,
+  //     //   groupMahou,
+  //     //   groupEsparte,
+  //     //   groupAguila,
+  //   ],
+  // });
 
-  var overlayMaps = {
-    Mahou: groupCruzcampo,
-    Cruzcampo: groupMahou,
-    Esparte: groupEsparte,
-    Aguila: groupAguila,
-    "?": baseGroup,
-  };
+  if (map === null) {
+    map = L.map("map", {
+      center: [36.3, -6.1],
+      zoom: 13, // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
+      minZoom: 11,
+      scrollWheelZoom: true,
+      tap: false,
+    });
+    Esri_WorldStreetMap.addTo(map);
 
-  var controlLayers = L.control
-    .layers(tileLayer, overlayMaps, {
-      position: "topright",
-      collapsed: true,
-    })
-    .addTo(map);
-  //.addTo(map);
-  //controlLayers.addBaseLayer(OpenStreetMap_Mapnik, "OpenStreetMap_Mapnik");
+    markersLayer.addTo(map);
+  }
 
-  // var Stamen_Watercolor = L.tileLayer(
-  //   "https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}",
+  // var overlayMaps = {
+  //   Mahou: groupCruzcampo,
+  //   Cruzcampo: groupMahou,
+  //   Esparte: groupEsparte,
+  //   Aguila: groupAguila,
+  //   "?": baseGroup,
+  // };
+
+  // var controlLayers = L.control
+  //   .layers(tileLayer, overlayMaps, {
+  //     position: "topright",
+  //     collapsed: true,
+  //   })
+  //   .addTo(map);
+
+  // var Stadia_AlidadeSmoothDark = L.tileLayer(
+  //   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
   //   {
+  //     maxZoom: 20,
   //     attribution:
-  //       'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  //     subdomains: "abcd",
-  //     minZoom: 1,
-  //     maxZoom: 16,
-  //     ext: "jpg",
+  //       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
   //   }
   // );
-  // controlLayers.addBaseLayer(Stamen_Watercolor, "Stamen_Watercolor");
+  // controlLayers.addBaseLayer(
+  //   Stadia_AlidadeSmoothDark,
+  //   "Stadia_AlidadeSmoothDark"
+  // );
 
-  var Stadia_AlidadeSmoothDark = L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-    {
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    }
-  );
-  controlLayers.addBaseLayer(
-    Stadia_AlidadeSmoothDark,
-    "Stadia_AlidadeSmoothDark"
-  );
-
-  var Stadia_AlidadeSmooth = L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-    {
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    }
-  );
-  controlLayers.addBaseLayer(Stadia_AlidadeSmooth, "Stadia_AlidadeSmooth");
+  // var Stadia_AlidadeSmooth = L.tileLayer(
+  //   "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+  //   {
+  //     maxZoom: 20,
+  //     attribution:
+  //       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  //   }
+  // );
+  // controlLayers.addBaseLayer(Stadia_AlidadeSmooth, "Stadia_AlidadeSmooth");
 
   function getPopupHtml(row) {
     let popupHtml = `<h3 style="text-align: center">${row.Bar}</h3>`;
@@ -179,6 +216,12 @@ $.get(file, function (csvString) {
   map.attributionControl.setPrefix(
     'View <a href="https://github.com/HandsOnDataViz/leaflet-map-csv" target="_blank">code on GitHub</a>'
   );
-});
+};
+
+const updateFilters = () => {
+  console.log("foooo");
+  markersLayer.clearLayers();
+  updateMap(dataCsv);
+};
 
 //add the groups of markers to layerGroups
