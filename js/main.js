826a4988fa756e572199a7be169b0917aef441fb
Jaime <jaime.chapinal@gmail.com>
2022-07-08 20:00:56 +0200

    Updating data

--- js/main.js
+++ js/main.js
@@ -3,37 +3,118 @@ var map = null;
 var markers = [];
 var markersLayer = new L.LayerGroup();
 
-var filterCruzcampo = document.getElementById("filterCruzcampo");
-var filterMahou = document.getElementById("filterMahou");
-var filterEstrellaGalicia = document.getElementById("filterEstrellaGalicia");
-var filterEsparte = document.getElementById("filterEsparte");
+let listBeers = [];
 
-var filterUnknown = document.getElementById("filterUnknown");
+let filtersElem = {};
 
-// var allMarkers = [];
-// var markersCruzcampo = [];
-// var markersMahou = [];
-// var markersEsparte = [];
-// var markersAguila = [];
+const createUtilitySelectionDiv = () => {
+  debugger;
+  const divSelectAll = document.createElement("div");
+  divSelectAll.classList.add("filterBrand2222");
+  const inputElement = document.createElement("input");
+  inputElement.id = "selectAll";
+  inputElement.type = "checkbox";
+  //filtersElem[beer] = inputElement;
+
+  const labelElement = document.createElement("label");
+  labelElement.setAttribute("for", "selectAll");
+  labelElement.innerHTML = "Seleccionar todos";
+
+  divSelectAll.appendChild(inputElement);
+  divSelectAll.appendChild(labelElement);
+
+  const divUnselectAll = document.createElement("div");
+  divUnselectAll.classList.add("filterBrand2222");
+  const inputUnselectElement = document.createElement("input");
+  inputUnselectElement.id = "unselectAll";
+  inputUnselectElement.type = "checkbox";
+  //filtersElem[beer] = inputUnselectElement;
+
+  const labelUnselect = document.createElement("label");
+  labelUnselect.setAttribute("for", "selectAll");
+  labelUnselect.innerHTML = "Eliminar seleccion";
+
+  divUnselectAll.appendChild(inputUnselectElement);
+  divUnselectAll.appendChild(labelUnselect);
+
+  const divUtility = document.createElement("div");
+  divUtility.classList.add("filterBrand2222");
+  divUtility.appendChild(divSelectAll);
+  divUtility.appendChild(divUnselectAll);
+};
+
+const createFilterDiv = (beer, customLabel = "") => {
+  const div = document.createElement("div");
+  div.classList.add("filterBrand");
+
+  const inputElement = document.createElement("input");
+  inputElement.id = beer;
+  inputElement.type = "checkbox";
+  inputElement.checked = !customLabel;
+  filtersElem[beer] = inputElement;
+
+  const labelElement = document.createElement("label");
+  labelElement.setAttribute("for", beer);
+  labelElement.innerHTML = customLabel ? customLabel : beer;
+
+  div.appendChild(inputElement);
+  div.appendChild(labelElement);
+  return div;
+};
+
+const createDynamicFiltersFieldSet = (listBeers) => {
+  const anchorElement = document.getElementById("dynamicFieldSet");
+  for (beer of listBeers) {
+    anchorElement.appendChild(createFilterDiv(beer));
+  }
+  anchorElement.appendChild(createFilterDiv("Unknown", "❓"));
+
+  //anchorElement.append(createUtilitySelectionDiv());
+};
+
+const isValidCervezasRow = (field) => {
+  return field !== "-" && field !== ".";
+};
+
+const getListOfDifferentBeers = (data) => {
+  let listBeers = [];
+  for (row of data) {
+    if (row.cervezas && isValidCervezasRow(row.cervezas)) {
+      console.log(row + " ---> " + row.cervezas);
+      const availableBeersInBar = Object.getOwnPropertyNames(
+        JSON.parse(row.cervezas)
+      );
+      for (beer of availableBeersInBar) {
+        if (!listBeers.includes(beer)) {
+          listBeers.push(beer);
+        }
+      }
+    }
+  }
+  return listBeers;
+};
 
 const file = "./data/conil.csv";
-$.get(file, function (csvString) {
-  var data = Papa.parse(csvString, {
-    header: true,
-    dynamicTyping: true,
-  }).data;
+const response = fetch(file)
+  .then((response) => response.text())
+  .then((v) => {
+    var data = Papa.parse(v, {
+      header: true,
+      dynamicTyping: true,
+    }).data;
 
-  dataCsv = data;
+    dataCsv = data;
 
-  updateMap(dataCsv);
-});
+    listBeers = getListOfDifferentBeers(data);
+    listBeers.sort();
+    createDynamicFiltersFieldSet(listBeers);
+
+    updateMap(dataCsv);
+  })
+  .catch((err) => console.log(err));
 
 const updateMap = (data) => {
-  const isCruzcampoEnabled = filterCruzcampo.checked;
-  const isMahouEnabled = filterMahou.checked;
-  const isEstrellaGaliciaEnabled = filterEstrellaGalicia.checked;
-  const isEsparteEnabled = filterEsparte.checked;
-  const isUnknownEnabled = filterUnknown.checked;
+  filtersElem[beer];
 
   for (var i in data) {
     var row = data[i];
@@ -52,40 +133,15 @@ const updateMap = (data) => {
           icon: myIcon,
         }).bindPopup(popupHtml);
 
-        // if (hasBeerData) {
-        //   if (popupHtml.includes("Cruzcampo")) {
-        //     markersCruzcampo.push(marker);
-        //   }
-        //   if (popupHtml.includes("Mahou")) {
-        //     markersMahou.push(marker);
-        //   }
-        //   if (popupHtml.includes("Esparte")) {
-        //     markersEsparte.push(marker);
-        //   }
-        //   if (popupHtml.includes("Aguila")) {
-        //     markersAguila.push(marker);
-        //   }
-        // } else {
-        //   allMarkers.push(marker);
-        // }
         if (hasBeerData) {
-          if (isCruzcampoEnabled && popupHtml.includes("Cruzcampo")) {
-            shouldAddToMap = true;
-          }
-          if (isMahouEnabled && popupHtml.includes("Mahou")) {
-            shouldAddToMap = true;
-          }
-          if (isEsparteEnabled && popupHtml.includes("Esparte")) {
-            shouldAddToMap = true;
-          }
-          if (
-            isEstrellaGaliciaEnabled &&
-            popupHtml.includes("Estrella Galicia")
-          ) {
-            shouldAddToMap = true;
+          for (beer of listBeers) {
+            const isFilterEnabled = filtersElem[beer].checked;
+            if (isFilterEnabled && popupHtml.includes(beer)) {
+              shouldAddToMap = true;
+            }
           }
         } else {
-          shouldAddToMap = isUnknownEnabled;
+          shouldAddToMap = filtersElem["Unknown"].checked;
         }
 
         if (shouldAddToMap) {
@@ -108,12 +164,6 @@ const updateMap = (data) => {
     }
   }
 
-  // let baseGroup = L.layerGroup(allMarkers);
-  // let groupCruzcampo = L.layerGroup(markersCruzcampo);
-  // let groupMahou = L.layerGroup(markersMahou);
-  // let groupEsparte = L.layerGroup(markersEsparte);
-  // let groupAguila = L.layerGroup(markersAguila);
-
   var Esri_WorldStreetMap = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
     {
@@ -121,25 +171,6 @@ const updateMap = (data) => {
         "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012",
     }
   );
-  //controlLayers.addBaseLayer(Esri_WorldStreetMap, "Esri_WorldStreetMap");
-  var tileLayer = { BaseLayer: Esri_WorldStreetMap };
-
-  // map = L.map("map", {
-  //   center: [36.3, -6.1],
-  //   zoom: 13, // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
-  //   minZoom: 11,
-  //   scrollWheelZoom: true,
-  //   tap: false,
-  //   layers: [
-  //     tileLayer["BaseLayer"],
-  //     //   baseGroup,
-  //     //   groupCruzcampo,
-  //     //   groupMahou,
-  //     //   groupEsparte,
-  //     //   groupAguila,
-  //   ],
-  // });
-
   if (map === null) {
     map = L.map("map", {
       center: [36.3, -6.1],
@@ -152,44 +183,6 @@ const updateMap = (data) => {
 
     markersLayer.addTo(map);
   }
-
-  // var overlayMaps = {
-  //   Mahou: groupCruzcampo,
-  //   Cruzcampo: groupMahou,
-  //   Esparte: groupEsparte,
-  //   Aguila: groupAguila,
-  //   "?": baseGroup,
-  // };
-
-  // var controlLayers = L.control
-  //   .layers(tileLayer, overlayMaps, {
-  //     position: "topright",
-  //     collapsed: true,
-  //   })
-  //   .addTo(map);
-
-  // var Stadia_AlidadeSmoothDark = L.tileLayer(
-  //   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-  //   {
-  //     maxZoom: 20,
-  //     attribution:
-  //       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-  //   }
-  // );
-  // controlLayers.addBaseLayer(
-  //   Stadia_AlidadeSmoothDark,
-  //   "Stadia_AlidadeSmoothDark"
-  // );
-
-  // var Stadia_AlidadeSmooth = L.tileLayer(
-  //   "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-  //   {
-  //     maxZoom: 20,
-  //     attribution:
-  //       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-  //   }
-  // );
-  // controlLayers.addBaseLayer(Stadia_AlidadeSmooth, "Stadia_AlidadeSmooth");
 
   function getPopupHtml(row) {
     let popupHtml = `<h3 style="text-align: center">${row.Bar}</h3>`;
@@ -219,9 +212,6 @@ const updateMap = (data) => {
 };
 
 const updateFilters = () => {
-  console.log("foooo");
   markersLayer.clearLayers();
   updateMap(dataCsv);
 };
-
-//add the groups of markers to layerGroups
